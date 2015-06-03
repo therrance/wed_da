bbc1942efc4bc5ca2763d05596433e27ab92987a
therrance <lozuvatka@gmail.com>
2015-06-03 11:53:43 +0000

    init

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,15 @@
-angular
-  .module('thinkster', []);
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster', [
+      'thinkster.routes',
+      'thinkster.authentication'
+    ]);
+
+  angular
+    .module('thinkster.config', []);    
+
+  angular
+    .module('thinkster.routes', ['ngRoute']);
+})();
